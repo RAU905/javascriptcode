0adc0a96ea4da3d4cd9ce55642a971e6dd28b5d1
Raushan kumar <111661802+RAU905@users.noreply.github.com>
2023-02-22 17:37:13 +0530

    Add files via upload
    JAVSCRIPT CODE [promise.all  ,async await ] concepts

--- win.js
+++ win.js
@@ -1,129 +1,131 @@
-// console.log("heloo world it is mmy vonsoe");
+// let p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(1111);
+//   }, 1000);
+// });
+// try {
+//   console.log(tom);
+// } catch (er) {
+//   console.log(" your error is " + er);
+// }
+// let p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(222);
+//   }, 8000);
+// });
+// let p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(3333);
+//   }, 2000);
+// });
+// let y = prompt(" enter your age ");
+// y = Number.parseInt(y);
+// if (y > 190) {
+//   try {
+//     throw new Error(" it is ");
+//   } catch (err) {
+//     console.log(" the erroe is :-" + err);
+//     console.log(" the erroe is :-" + err.name);
 
-// // setTimeout( () => {
-// //   console.log( a+b);
-// // alert ("hi i am alert ")
-// // console.log(sum(11,12))
-// // }, 2000);
-// // setInterval(() => {
-// //   alert("hhi iam setinterval");
-// // }, 2000);
-// // setTimeout(sum, 2000, 11, 23);
-// // const sum = (a, b) => {
-// //   alert("i am  add");
-// //   a + b;
-// // };
-
-// // setInterval(sum,3000,12185,2154)
-// let b = document.getElementsByClassName("sss")[0];
-// b.onclick = () => {
-//   alert("hi iam am alerat ");
-// };
-// let x = function (e) {
-//   alert("helloworld ");
-// };
-// let y = function (e) {
-//   alert("hi ");
-// };
-
-// aws.addEventListener("click", x);
-// aws.addEventListener("click", y);
-// let a = prompt("enter any numebr");
-
-// if (a == 123) {
-//   aws.removeEventListener("click", x);
+//     console.log(" the erroe is :-" + err.message);
+//     console.log(" the erroe is :-" + err.stack);
+//   }
+// } else {
+//   console.log(" yes your age is valid");
 // }
 
-// setInterval(async function () {
-//   document.getElementsByClassName("#yello").classlist.toggle("yello");
-// }, 1000);
+// let loadscript = async (src) => {
+//   return new Promise((resolve, reject) => {
+//     let script = document.createElement("script");
+//     script.src = src;
+//     script.onload = () => {
+//       resolve(src);
+//       console.log(" your script is " + src);
+//     };
+//     document.head.append(script);
+//   });
+// };
+// let helo = async () => {
+//   let a = await loadscript(
+//     "https://drive.google.com/drive/folders/18_zQbPP3VgdK81Yl_WpFaH2OVKTxPzN0"
+//   );
+//   console.log(a);
+// };
+// helo().then(alert(" yes you have done"));
 
-// let a = new Date();
+// let du = async () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       reject(new Error(" this is an error "));
+//     }, 3000);
+//   });
+// };
+
+// let a = async () => {
+//   try {
+//     let c = await du();
+//     console.log(c);
+//   } catch (err) {
+//     console.log(" your error is " + err);
+//   }
+// };
+
+// a().then(alert(" donne it is "));
+
+let p1 = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("i am p1");
+    }, 2000);
+  });
+};
+let p2 = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("i am p2");
+    }, 3000);
+  });
+};
+let p3 = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("i am p3");
+    }, 4000);
+  });
+};
+
+const fun2 = async () => {
+  console.time("fun2");
+
+  let a = await p1();
+  console.log(a);
+
+  let b = await p2();
+  console.log(b);
+
+  let c = await p3();
+  console.log(c);
+  // let a = p1();
+  // let b = p2();
+  // let c = p3();
+
+  // let d = await Promise.all([a, b, c]);
+  // console.log(d);
+  // let d = await Promise.all([a, b, c]);
+  // console.log(d);
+
+  console.timeEnd("fun2");
+};
+
+fun2();
+
+// p1.then(console.log(" p1 done"));
+// let a = await p1();
 // console.log(a);
-// let h = a.getHours;
-// let m = a.getMinutes;
-// let s = a.getSeconds;
-// let d = a.getDate;
-// console.log(h, m, s, d);
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  realTime();
-});
+// let b = await p2();
+// console.log(b);
 
-function realTime() {
-  var date = new Date();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
-  var date = date.getDate();
-  // var month = date.getMonth();
-  // var year = date.getyear();
-
-  var halfday = "AM";
-  halfday = hour >= 12 ? "PM" : "AM";
-  hour = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
-  hour = update(hour);
-  min = update(min);
-  sec = update(sec);
-  date = update(date);
-  // month = update(month);
-  // year = update(year);
-
-  document.getElementById("h").innerText = hour;
-  document.getElementById("m").innerText = min;
-  document.getElementById("s").innerText = sec;
-  document.getElementById("da").innerText = date;
-  // document.getElementById("mo").innerText = month;
-  // document.getElementById("ye").innerText = year;
-
-  document.getElementById("ap").innerText = halfday;
-
-  setTimeout(realTime, 1000);
-}
-
-function update(k) {
-  if (k < 10) {
-    return "0" + k;
-  } else {
-    return k;
-  }
-}
-
-let promises = new Promise((resolve, reject) => {
-  alert("hi iam alert under promises");
-  resolve(44);
-});
-
-console.log("hi hello");
-setTimeout(function () {
-  console.log(" ggood evening ");
-}, 1000);
-
-console.log(" i am second after settimeout");
-console.log(promises);
-
-let p1 = new Promise((resolve, reject) => {
-  resolve("value 1");
-});
-
-let p2 = new Promise((resolve, reject) => {
-  resolve("value 2");
-});
-let p3 = new Promise((resolve, reject) => {
-  resolve("value 3");
-});
-
-// p1.then((value) => {
-//   console.log(value);
-// });
-// p2.then((value) => {
-//   console.log(value);
-// });
-// p3.then((value) => {
-//   console.log(value);
-// });
-
-let promisea = Promise.all([p1, p2, p3]);
-promisea.then((value) => {
-  console.log(value);
-});
+// let c = await p3();
+// console.log(c);
+// p1.then(console.log(" p1 done"));
